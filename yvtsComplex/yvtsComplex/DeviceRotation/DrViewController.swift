7b35136119b204470adefce5a58b212658ff3489
dvnik147 <dvnik147@hotmail.com>
2020-10-15 23:15:25 +0800

    dr view set

---
+++ yvtsComplex/yvtsComplex/DeviceRotation/DrViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DrViewController.swift
+//  yvtsComplex
+//
+//  Created by Trixie Lulamoon on 2020/10/15.
+//
+
+import UIKit
+
+class DrViewController: UIViewController
+{
+    // values
+    var deviceWidth:CGFloat = 0
+    var deviceHeight:CGFloat = 0
+    // UI Objects
+    @IBOutlet weak var imgPic: UIImageView!
+    
+    
+    
+    //MARK: - my Functions
+    
+    //MARK: - View Life Cycle
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - Target Actions
+    @IBAction func doAnimation(_ sender: UIButton)
+    {
+        
+    }
+    
+}
